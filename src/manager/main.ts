90696178cb77df3ef30ee06acee749d31c4b1297
subframe7536 <1667077010@qq.com>
2024-10-18 12:44:05 +0800

    feat: custom electron background color

--- src/manager/main.ts
+++ src/manager/main.ts
@@ -6,6 +6,10 @@ const entry = 'backgroundColor:r.getBackgroundColor()'
 
 export class MainFileManager extends BaseFileManager {
   patch(_fontChanged: boolean, content: () => string): Promisable<string | undefined> {
-    return content().replace(entry, `${JSON.stringify(config.electron).slice(1, -1)},${entry}`)
+    const backgroundColor = config.electron.backgroundColor || 'r.getBackgroundColor()'
+    return content().replace(
+      entry,
+      `${JSON.stringify(config.electron).slice(1, -1)},backgroundColor:${backgroundColor}`,
+    )
   }
 }
