ceabfde9b0dc32958ea23cea0f711eece3a00f3f
subframe7536 <1667077010@qq.com>
2024-12-26 12:37:31 +0800

    chore: upgrade deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,10 @@ import { defineEslintConfig } from '@subframe7536/eslint-config'
 
 export default defineEslintConfig({
   type: 'app',
+  ignoreRuleOnFile: {
+    files: '**/*.md/*.jsonc',
+    rules: [
+      'jsonc/comma-dangle',
+    ],
+  },
 })
