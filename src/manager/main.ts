cfa266cb00f67c67ead1d78f24d074b23a012914
subframe7536 <1667077010@qq.com>
2025-01-10 08:59:57 +0800

    feat: override setBackgroundColor #20

--- src/manager/main.ts
+++ src/manager/main.ts
@@ -3,6 +3,7 @@ import { mainBakPath, mainPath } from '../path'
 import { BaseFileManager } from './base'
 
 const entry = 'experimentalDarkMode:!0'
+const setBgColorRegex = /setBackgroundColor\([\w.]+\);/g
 
 export class MainFileManager extends BaseFileManager {
   constructor() {
@@ -10,9 +11,16 @@ export class MainFileManager extends BaseFileManager {
   }
 
   patch(content: string): string {
-    return content.replaceAll(
+    let patched = content.replaceAll(
       entry,
       `${entry},${JSON.stringify(config.electron).slice(1, -1)}`,
     )
+    if ('backgroundColor' in config.electron) {
+      patched = patched.replace(
+        setBgColorRegex,
+        `setBackgroundColor("${config.electron.backgroundColor}");`,
+      )
+    }
+    return patched
   }
 }
